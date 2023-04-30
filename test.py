46b65852f46cab9d8de8e5a9d24527efe914ffeb
ElizavetaShangina <118834113+ElizavetaShangina@users.noreply.github.com>
2023-04-30 11:18:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,41 @@
 import sys
-from io import BytesIO
+import pygame
 import requests
-from PIL import Image
 from map_settings import map_setting
+import random
 
 
-toponym_to_find = " ".join(sys.argv[1:])
-map_params = map_setting(toponym_to_find)
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-Image.open(BytesIO(
-    response.content)).show()
+def show_picture(i, map_params):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params[i % len(map_params)])
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+
+
+cities = ['Москва', 'Париж', 'Нью-Йорк', 'Нью-Дэли']
+city = cities[random.randint(0, 3)]
+map_params = map_setting(city)
+
+if not map_params:
+    print('ERROR with map')
+    exit()
+
+i, c, fps = -1, -1, 1
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+clock = pygame.time.Clock()
+for event in pygame.event.get():
+    if event.type == pygame.QUIT: pygame.quit()
+    else:
+        i += 1
+        show_picture(i, map_params)
+        clock.tick(fps)
